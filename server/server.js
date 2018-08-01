eac6610b732e90e6f2b65912600c8659f6e2c224
mahmoud-hany <mahmoud.hany.5220@gmail.com>
2018-08-01 22:06:37 +0200

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//express
+const { ObjectID } = require('mongodb'); 
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//Make todos
 app.post('/todos', (req, res) => {
     const newTodo = new Todo({
        text: req.body.text
@@ -23,7 +24,7 @@ app.post('/todos', (req, res) => {
     });
 });
 
-//fetch all the todos 
+//Get All Todos 
 app.get('/todos', (req, res) => {
     Todo.find().then(todos => {
         res.send({ todos }); //todos is array
@@ -31,6 +32,29 @@ app.get('/todos', (req, res) => {
         res.status(400).send(error); 
     })
 })
+
+//Get Todo by id [id is variable]
+//req params is object contains our varaiables
+app.get(`/todos/:id`, (req, res) => {
+    // res.send(req.params); 
+    const ID = req.params.id;
+
+    if(! ObjectID.isValid(ID) ){
+        return res.status(400).send(); //send status NotFound: 404
+    }
+
+    Todo.findById( ID ).then( todo => {
+        if(!todo){
+            return res.status(404).send(); //send status badRequest: 400
+        }
+        
+        res.status(200).send({todo, status: "OK"}); //send status Ok: 200
+    }).catch(error => {
+        console.log('Unable to find thid one');
+        res.status(400).send(error) //send status badRequest: 400
+    })
+
+});
 
 //server listen
 app.listen(3000, () => {
